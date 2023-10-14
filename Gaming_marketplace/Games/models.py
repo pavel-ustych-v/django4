3134e96ee70823eb3d1757e537d9b3c53012decf
pavel-ustich-v <tulenforpython1@gmail.com>
2023-10-14 18:40:27 +0300

    ok now this

--- Gaming_marketplace/Games/models.py
+++ Gaming_marketplace/Games/models.py
@@ -5,5 +5,5 @@ class Game(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     creator = models.CharField(max_length=255)
-    genre = models.CharField()
+    genre = models.CharField(max_length=255)
     year = models.IntegerField()
